b4351b199ec097d518b2366ec12fa071c6f36328
Jason <jason.novacovici@gmail.com>
2022-04-26 13:23:35 -0400

    Complete getNextUniqueId

--- datastore/counter.js
+++ datastore/counter.js
@@ -38,12 +38,14 @@ const writeCounter = (count, callback) => {
 
 // Public API - Fix this function //////////////////////////////////////////////
 
-exports.getNextUniqueId = () => {
-  counter = counter + 1;
-  return zeroPaddedNumber(counter);
+exports.getNextUniqueId = (callback) => {
+  readCounter((err, fileNum) => {
+    counter = fileNum;
+    counter = counter + 1;
+    writeCounter(counter, callback);
+  });
+  // return zeroPaddedNumber(counter);
 };
-
-
 
 // Configuration -- DO NOT MODIFY //////////////////////////////////////////////
 
